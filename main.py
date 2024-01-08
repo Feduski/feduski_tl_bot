d3d3013753636fffb3fe65328b063ecccc2fab7b
Federico <Fedengp@gmail.com>
2024-01-08 15:32:07 -0300

    dont ask, it works

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import os
 import json
-import requests
-import schedule
-import logging 
-import datetime
-import time as t
-from pyowm.owm import OWM
+import pytz
 import logging
-from dotenv import load_dotenv
+import requests
+from pyowm.owm import OWM
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
-chat_ids = []
+from dotenv import load_dotenv
+from datetime import time, datetime, date
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackContext
 
 load_dotenv()
 tl_key = os.getenv('TELEGRAM_KEY')
+chat_id = os.getenv('CHAT_ID')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -23,35 +20,10 @@ logging.basicConfig(
 def error(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
     logging.error(f'Update "{update}" caused error {ctx.error}')
 
-#Needed for dolar value report
 data = (requests.get('https://api.bluelytics.com.ar/v2/latest')).json()
 actual_value = data['blue']['value_sell']
 
-def send_msg(bot_message):
-    for chat_id in chat_ids:
-        send_text = 'https://api.telegram.org/bot' + tl_key + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
-        response = requests.get(send_text)
-        return response.json()
-
-def dolar_close(): #should send the message to every chat
-    close = ((requests.get('https://api.bluelytics.com.ar/v2/latest')).json())['blue']['value_sell']
-    send_msg(f'The dollar has closed at ${close}💸')
-
-def proof():
-    send_msg('This is a proof message')
-
-def check_dollar_update(): #should send the message to every chat
-    global actual_value
-    new_value = ((requests.get('https://api.bluelytics.com.ar/v2/latest')).json())['blue']['value_sell']
-
-    if new_value > actual_value:
-        send_msg(f'The dollar value rose to {new_value}📈') 
-    elif new_value < actual_value:
-        send_msg(f'The dollar value fell to {new_value}📉') 
-    elif new_value == actual_value:
-        send_msg(f'The value of the dollar is still at {new_value}💹') #see how to send it to every chat, at X time
-    
-    actual_value = new_value
+#FUNCTIONS
 
 def weather():
     owm = OWM(os.getenv('OWM'))
@@ -63,16 +35,46 @@ def weather():
 
     return f'It is {weather_now.detailed_status}, with a minimun of {round(min)}°, a max of {round(max)}°, and a feels like of {round(fl)}° 🌥'
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.message.chat_id
-    if chat_id not in chat_ids:
-        chat_ids.append(chat_id)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello Sir, welcome! I can serve you the weather, the dolar value and your favourites cryptocurrencies values.")
-    logging.info(f'User ({update.message.chat.first_name}) executed: start_command, chat_id: {chat_id}')
+#SCHEDULED MESSAGES
 
+async def check_dollar_update(context: ContextTypes.DEFAULT_TYPE): #should send the message to every chat
+    global actual_value
+    new_value = ((requests.get('https://api.bluelytics.com.ar/v2/latest')).json())['blue']['value_sell']
+    diff = (actual_value - new_value)* 100 / actual_value
+
+    if new_value > actual_value:
+        await context.bot.send_message(chat_id=chat_id, text=f'The dollar value is up to {new_value}, ({diff})📈')
+    elif new_value < actual_value:
+        await context.bot.send_message(chat_id=chat_id, text=f'The dollar value is down to {new_value}, ({diff})📉')
+    
+    actual_value = new_value
+
+async def dolar_close(context: CallbackContext):
+    close = ((requests.get('https://api.bluelytics.com.ar/v2/latest')).json())['blue']['value_sell']
+    await context.bot.send_message(chat_id=chat_id, text=f'The dollar has closed at {close}💸')
+
+async def good_morning(context: CallbackContext):
+    dollar = ((requests.get('https://api.bluelytics.com.ar/v2/latest')).json())['blue']['value_sell']
+    today = (date.today()).strftime("%B %d, %Y")
+    await context.bot.send_message(chat_id=chat_id, text=f"""
+                                   \nGood Morning Sir. 
+                                   \nToday is {today}📅
+                                   \n{weather()}
+                                   \nThe dollar value is of ${dollar} 💸
+                                   \nHave a nice day 🤖
+                                   """)
+
+#COMMANDS
+async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello Sir, welcome! I can serve you the weather, the dolar value and your favourites cryptocurrencies values.")
+    logging.info(f'User: ({update.message.chat.first_name}) executed: start_command.')
+
+async def check_command(update: Update, context: ContextTypes.DEFAULT_TYPE): 
+    await context.bot.send_message(chat_id=context._user_id ,text='Im alive, check command')
+    logging.info(f'User: ({update.message.chat.first_name}) executed: check_command.')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("You picked out the help command, how can i help u?\nHere are all my commands for now:\nstart - Find out what this bot can do🐍\nhelp - Get assistance for this bot⛑\ndolar - Get the value of dolar to Pesos argentinos💸\ncryptos - Get the actual value of my favourites cryptos🔥\nweather - Get what's the weather like right now🌥")
+    await update.message.reply_text("You picked out the help command, how can i help u?\nHere are all my commands for now:\nstart - Find out what this bot can do🐍\nhelp - Get assistance for this bot⛑\ndolar - Get the value of dolar to Pesos argentinos💸\ncryptos - Get the actual value of my favourites cryptos🪙\nweather - Get what's the weather like right now🌥")
     logging.info(f'User ({update.message.chat.first_name}) executed: help_command')
 
 async def dollar_command(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
@@ -89,36 +91,26 @@ async def crypto_command(update, ctx):
     consultaETH = json.loads(requests.get(f'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD').text)["USD"]
     consultaDAI = (requests.get('https://criptoya.com/api/lemoncash/DAI').json())['ask']
 
-    await update.message.reply_text(f'\tActual Value 💹\n🔥 BTC:   {consultaBTC}\n🔗 ETH:   {consultaETH}\n💲 DAI:   {consultaDAI}')
+    await update.message.reply_text(f'\tActual prices👇🏼\n⚡️ BTC/USD: {consultaBTC}\n⛓ ETH/USD: {consultaETH}\n💵 DAI/ARS: {consultaDAI}')
     logging.info(f'User ({update.message.chat.first_name}) executed: cryptos_command')
 
-if __name__ == '__main__':
+application = Application.builder().token(tl_key).build()
+job_queue = application.job_queue
 
-    app = ApplicationBuilder().token(tl_key).build()
-    
-    app.add_handler(CommandHandler('start', start))
-    app.add_handler(CommandHandler('dollar', dollar_command))    
-    app.add_handler(CommandHandler('help', help_command))    
-    app.add_handler(CommandHandler('weather', weather_command))    
-    app.add_handler(CommandHandler('crypto', crypto_command))    
+application.add_handler(CommandHandler('start', start_command))
+application.add_handler(CommandHandler('check', check_command))
+application.add_handler(CommandHandler('help', help_command))
+application.add_handler(CommandHandler('dollar', dollar_command))
+application.add_handler(CommandHandler('weather', weather_command))
+application.add_handler(CommandHandler('crypto', crypto_command))
 
-    app.add_error_handler(error)
-    app.run_polling(poll_interval=5.0)
+application.add_error_handler(error)
 
-    schedule.every().hour.do(check_dollar_update)
-    schedule.every(1).minutes.do(proof) #not working too2
+job_minute = job_queue.run_repeating(check_dollar_update, interval=1800, first=10)
 
-    schedule.run_pending()
-    t.sleep(1)
+buenos_aires_tz = pytz.timezone('America/Argentina/Buenos_Aires')
 
+job_daily = job_queue.run_daily(dolar_close, time=(time(17,00, tzinfo=buenos_aires_tz)), days=(1, 2, 3, 4, 5))
+job_daily = job_queue.run_daily(good_morning, time=(time(15,27, tzinfo=buenos_aires_tz)))
 
-"""
-#Auto-reply
-def handle_message(update, ctx):
-    text = str(update.message.text)
-    update.message.reply_text(text)
-    logging.info(f'User ({update.message.chat.first_name}) says: {text}')
-"""
-
-#TODO: Fix schedule and the time of the messages 
-#Is chait_ids implemented right?
+application.run_polling(poll_interval=10.0)
